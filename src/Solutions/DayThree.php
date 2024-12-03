db5e1f6819bdd5a24c4ece2f2171cd87ff961db1
Vaugen Wakeling <55480610+vaugenwake@users.noreply.github.com>
2024-12-03 20:46:16 +0000

    Day 3 pt2

--- src/Solutions/DayThree.php
+++ src/Solutions/DayThree.php
@@ -4,7 +4,8 @@ namespace Vaugenwakeling\Aoc2025\Solutions;
 
 class DayThree
 {
-    const INPUT_FILE_NAME_PT1 = "./data/day3/input.txt";
+    const INPUT_FILE_NAME_PT1 = "./data/day3/pt1/input.txt";
+    const INPUT_FILE_NAME_PT2 = "./data/day3/pt2/input.txt";
 
     public function partOne(string $filename = self::INPUT_FILE_NAME_PT1) {
         $input = file_get_contents($filename);
@@ -35,7 +36,44 @@ class DayThree
         return array_sum($totals);
     }
 
-    public function partTwo(string $filename = self::INPUT_FILE_NAME_PT1) {
-        return 'Noop';
+    public function partTwo(string $filename = self::INPUT_FILE_NAME_PT2) {
+        $input = file_get_contents($filename);
+
+        $matches = [];
+
+        preg_match_all("/mul\(\d+,\d+\)|don't\(\)|do\(\)/i", $input, $matches);
+
+        if (empty($matches)) {
+            throw new \Exception("No matches found for input data");
+        }
+
+        $values = [];
+        $skip = false;
+        foreach (current($matches) as $match) {
+
+            if ($match == "do()") {
+                $skip = false;
+                continue;
+            }
+
+            if ($skip || $match == "don't()" || $match == "dont()") {
+                $skip = true;
+                continue;
+            }
+
+            $match = str_replace('mul', '', $match);
+            $match = str_replace('(', '', $match);
+            $match = str_replace(')', '', $match);
+
+            $values[] = explode(",", $match);
+        }
+
+        $totals = [];
+
+        foreach ($values as $value) {
+            $totals[] = $value[0]*$value[1];
+        }
+
+        return array_sum($totals);
     }
 }
--- tests/DayThreeTest.php
+++ tests/DayThreeTest.php
@@ -6,7 +6,16 @@ class DayThreeTest extends \PHPUnit\Framework\TestCase
     {
         $expected = 161;
 
-        $result = (new \Vaugenwakeling\Aoc2025\Solutions\DayThree())->partOne('./data/day3/sample.txt');
+        $result = (new \Vaugenwakeling\Aoc2025\Solutions\DayThree())->partOne('./data/day3/pt1/sample.txt');
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDayThreePartTwo(): void
+    {
+        $expected = 48;
+
+        $result = (new \Vaugenwakeling\Aoc2025\Solutions\DayThree())->partTwo('./data/day3/pt2/sample.txt');
 
         $this->assertEquals($expected, $result);
     }
